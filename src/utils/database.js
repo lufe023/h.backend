eae0b3aae5652d87797bd0f72f7bca7473b72879
Luis Fernando <lufe023@gmail.com>
2024-05-21 10:50:56 -0400

    resolviendo temas de ssl y conexion de DB y sequelize

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,12 +1,21 @@
-const { Sequelize } = require('sequelize')
-const config = require('../config')
+const { Sequelize } = require("sequelize");
+const config = require("../config");
 
 const db = new Sequelize({
-    dialect: 'postgres',
+    dialect: "postgres",
     host: config.db.host, //? Variable de entorno del host
     username: config.db.username, //? Variable de entorno del usuario
     password: config.db.password, //? Variable de entorno de la contraseña
-    database: config.db.dbName //? Variable de entorno de la base de datos
-})
+    database: config.db.dbName, //? Variable de entorno de la base de datos
+    dialectOptions:
+        process.env.NODE_ENV === "production"
+            ? {
+                  ssl: {
+                      require: true,
+                      rejectUnauthorized: false,
+                  },
+              }
+            : {},
+});
 
-module.exports = db
+module.exports = db;
